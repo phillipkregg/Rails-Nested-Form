ddcd3fe5040bbe0c2b128f4ad53cf8a991af83cc
PhillipKregg <phillipkregg@gmail.com>
2012-05-15 12:56:24 -0500

    First draft of Surveys application with nested model forms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailscastNestedModelForm::Application.config.secret_token = '9d4431e6eb865c6795ae817794bc4c382bca51ed3e557c2abe0be1e83fdaa077d2ca16915d34e6c57ad3d4586bcdc02fc9c60aa520fc8d7ab4796a6f9b0222a7'
